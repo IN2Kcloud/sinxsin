5a0f47b9d94b47cdc4c8d09231a08eda2c0d5375
IN2Kcloud <131200320+IN2Kcloud@users.noreply.github.com>
2023-06-22 21:50:14 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 gsap.registerPlugin(ScrollTrigger);
-
 const canvas = document.getElementById("sin-intro");
 const context = canvas.getContext("2d");
 canvas.width = 1920;
@@ -8,41 +7,28 @@ const frameCount = 300;
 const currentFrame = index => (
   `./result3/male${(index + 1).toString().padStart(4, '0')}.png`
 );
-const images = [];
+const images = []
 const sinxsin = {
   frame: 0
 };
-
-let imagesLoaded = 0;
-
 for (let i = 0; i < frameCount; i++) {
   const img = new Image();
-  const imgSrc = currentFrame(i);
-
-  img.onload = () => {
-    imagesLoaded++;
-    if (imagesLoaded === frameCount) {
-      
-      gsap.to(sinxsin, {
-        frame: frameCount - 1,
-        snap: "frame",
-        scrollTrigger: {
-          scrub: 1
-        },
-        onUpdate: render
-      });
-    }
-  };
-
-  img.src = imgSrc;
+  img.src = currentFrame(i);
   images.push(img);
 }
-
+gsap.to(sinxsin, {
+  frame: frameCount - 1,
+  snap: "frame",
+  scrollTrigger: {
+    scrub: 1
+  },
+  onUpdate: render
+});
+images[0].onload = render;
 function render() {
   context.clearRect(0, 0, canvas.width, canvas.height);
   context.drawImage(images[sinxsin.frame], 0, 0);
 }
-
 gsap.timeline({
   scrollTrigger: {
     trigger: ".section-2",
